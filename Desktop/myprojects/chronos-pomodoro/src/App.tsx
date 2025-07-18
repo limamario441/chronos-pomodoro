2615abdc97641f28e511c1178e7d8cd0f026fb33
limamario441 <limamario441@gmail.com.br>
2025-07-17 22:33:29 -0300

    iniciando-projeto-react

---
+++ Desktop/myprojects/chronos-pomodoro/src/App.tsx
@@ -0,0 +1,11 @@
+import { Heading } from './components/Heading';
+import './styles/theme.css';
+import './styles/global.css';
+export function App() {
+  return (
+    <>
+      <Heading />
+      <p>This is a simple React application.</p>
+    </>
+  );
+}
